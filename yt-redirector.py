91b2c0c7107a29236c7b6e3e8b5b9e26f143fdf6
Francois Lebel <francoislebel@gmail.com>
2013-04-03 23:57:13 -0400

    Sticking to single quotes for strings.

--- yt-redirector.py
+++ yt-redirector.py
@@ -15,7 +15,7 @@ def query(query):
   tree = ElementTree.parse(xml)
   root = tree.getroot()
 
-  entry = root.find("{http://www.w3.org/2005/Atom}entry")
+  entry = root.find('{http://www.w3.org/2005/Atom}entry')
   media = entry.find('{http://search.yahoo.com/mrss/}group')
   player = media.find('{http://search.yahoo.com/mrss/}player')
   player_url = player.attrib['url']
@@ -23,6 +23,6 @@ def query(query):
   return redirect(player_url, 301)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
 
