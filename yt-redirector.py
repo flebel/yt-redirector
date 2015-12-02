2267736e29abeb4c08d38a1086aea96390293b91
Francois Lebel <francoislebel@gmail.com>
2015-12-01 18:46:56 -0800

    Migrated to YouTube API v3.

--- yt-redirector.py
+++ yt-redirector.py
@@ -2,32 +2,21 @@
 
 import argparse
 import ConfigParser
-import urllib2
-from xml.etree import ElementTree
+import requests
 
 from flask import Flask, redirect
 
 app = Flask(__name__)
 parser = argparse.ArgumentParser(description='Flask app that redirects to the most relevant YouTube video based on a given query.')
 parser.add_argument('--config', default='default.conf', dest='config_file', help='Configuration file', type=str)
-
-url = u'https://gdata.youtube.com/feeds/api/videos?q={query}&orderby=relevance&max-results=1&v=2'
-
+url = u'https://www.googleapis.com/youtube/v3/search?key={YOUR_API_KEY}&maxResults=1&order=relevance&part=snippet&q={QUERY}&safeSearch=moderate'
 
 @app.route('/<query>')
-def query(query, api_key):
-  video_url = url.format(QUERY=query, YOUR_API_KEY=api_key)
-  xml = urllib2.urlopen(video_url)
-  tree = ElementTree.parse(xml)
-  root = tree.getroot()
-
-  entry = root.find('{http://www.w3.org/2005/Atom}entry')
-  media = entry.find('{http://search.yahoo.com/mrss/}group')
-  player = media.find('{http://search.yahoo.com/mrss/}player')
-  player_url = player.attrib['url']
-
+def query(query):
+  video_url = url.format(QUERY=query, YOUR_API_KEY=API_KEY)
+  video_id = requests.get(video_url).json()['items'][0]['id']['videoId']
+  player_url = 'https://www.youtube.com/watch?v=%s' % (video_id,)
   return redirect(player_url, 301)
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
